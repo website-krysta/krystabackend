d18cbea875d1c958325c4240b3e163340d06b89c
website-krysta <website.krysta@gmail.com>
2023-03-30 17:04:40 +0530

    adding

--- krystabackend/settings.py
+++ krystabackend/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+# 43.205.253.201','krysta.unaux.com','www.krysta.unaux.com'
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -141,7 +143,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
